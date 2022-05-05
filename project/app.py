1d5545debc4fce0b2f288884220e9d8052339c8e
Folkert van Verseveld <folkert.van.verseveld@gmail.com>
2022-05-05 23:55:12 +0200

    fix line endings, start shorten

--- project/app.py
+++ project/app.py
@@ -1,15 +1,36 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 
 def create_app(): #config_filename):
     app = Flask(__name__)
     #app.config.from_pyfile(config_filename)
-    
+
+    urls = {}
+
     @app.route('/')
     def hello_world():
         return 'Hello, World!'
-        
+
     @app.route('/shorten', methods=['POST'])
     def shorten():
-        return jsonify(statusCode=400, data=[]), 400
-        
+        import re
+        content = request.json
+
+        if not 'url' in content:
+            return '', 400
+
+        link = content['url']
+
+        if not 'shortcode' in content:
+            code = 'abc123'
+        else:
+            code = content['shortcode']
+
+        if code in urls:
+            return '', 409
+        elif not re.match(r"^[a-zA-Z0-9_]{6}$", code):
+            return '', 412
+
+        urls[link] = code
+        return jsonify(statusCode=201, data={'shortcode': code}), 201
+
     return app
--- tests/test_flask.py
+++ tests/test_flask.py
@@ -2,7 +2,7 @@ from project import app
 
 def test_running():
     a = app.create_app()
-    
+
     with a.test_client() as c:
         res = c.get('/')
         assert res.status_code == 200
@@ -10,7 +10,7 @@ def test_running():
 
 def test_not_found():
     a = app.create_app()
-    
+
     with a.test_client() as c:
         res = c.get('/dope')
         assert res.status_code == 404
@@ -18,7 +18,30 @@ def test_not_found():
 
 def test_hello():
     a = app.create_app()
-    
+
     with a.test_client() as c:
         res = c.get('/')
         assert b"Hello, World!" in res.data
+
+
+def test_shorten():
+    a = app.create_app()
+
+    with a.test_client() as c:
+        res = c.post('/shorten', json={'url': 'https://www.energyworx.com/', 'shortcode': 'ewx123'})
+        assert res.status_code == 201
+
+
+def test_shorten_no_url():
+    a = app.create_app()
+
+    with a.test_client() as c:
+        res = c.post('/shorten', json={'shortcode': 'ewx123'})
+        assert res.status_code == 400
+
+def test_shorten_badcode():
+    a = app.create_app()
+
+    with a.test_client() as c:
+        res = c.post('/shorten', json={'url': 'https://www.energyworx.com/', 'shortcode': 'eWx1235'})
+        assert res.status_code == 412
