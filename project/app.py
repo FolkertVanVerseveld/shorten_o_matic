e80e8377d48b54e64f5e7efe48ace9d7f22ddb54
Folkert van Verseveld <folkert.van.verseveld@gmail.com>
2022-05-06 01:14:56 +0200

    complete api and unit testing

--- project/app.py
+++ project/app.py
@@ -1,4 +1,38 @@
-from flask import Flask, jsonify, request
+import datetime
+from flask import Flask, jsonify, request, redirect
+
+
+class MyUrl:
+    """wrapper for shortened urls that also tracks redirect count and last used time"""
+
+    def __init__(self, code, link):
+        self.code = code
+        self.link = link
+        self.created = datetime.datetime.now(datetime.timezone.utc)
+        self.lastRedirect = self.created
+        self.redirectCount = 0
+
+
+    def touch(self):
+        """use link"""
+        self.lastRedirect = datetime.datetime.now(datetime.timezone.utc)
+        self.redirectCount += 1
+        return redirect(self.link, code=302)
+
+
+    def stats(self):
+        return jsonify(created=to_iso8601(self.created), lastRedirect=to_iso8601(self.lastRedirect), redirectCount=self.redirectCount), 200
+
+
+def create_url(length=6):
+    """randomize shortened url"""
+    import string
+    import random
+
+    s = string.ascii_lowercase + string.ascii_uppercase + '_'
+
+    return ''.join(random.choice(s) for i in range(length))
+
 
 def create_app(): #config_filename):
     app = Flask(__name__)
@@ -10,27 +44,77 @@ def create_app(): #config_filename):
     def hello_world():
         return 'Hello, World!'
 
+
     @app.route('/shorten', methods=['POST'])
     def shorten():
         import re
         content = request.json
 
         if not 'url' in content:
-            return '', 400
+            return missing_field()
 
         link = content['url']
 
         if not 'shortcode' in content:
-            code = 'abc123'
+            code = create_url()
         else:
             code = content['shortcode']
 
         if code in urls:
-            return '', 409
+            return code_used()
         elif not re.match(r"^[a-zA-Z0-9_]{6}$", code):
             return '', 412
 
-        urls[link] = code
-        return jsonify(statusCode=201, data={'shortcode': code}), 201
+        urls[code] = MyUrl(code, link)
+        return jsonify(shortcode=code), 201
+
+
+    @app.route('/', defaults={'u_path': ''})
+    @app.route('/<path:u_path>')
+    def unshorten(u_path):
+        if u_path is None:
+            return not_found()
+
+        if '/' in u_path:
+            u_path, rest = u_path.split('/', maxsplit=2)
+            if rest == 'stats':
+                return unshorten_stats(u_path)
+            else:
+                return not_found()
+
+        if u_path not in urls:
+            return not_found()
+
+        return urls[u_path].touch()
+
+
+    def unshorten_stats(code):
+        if code not in urls:
+            return not_found()
+
+        return urls[code].stats()
 
     return app
+
+# error handling
+
+def missing_field():
+    return '', 400
+
+
+def not_found():
+    return '', 404
+
+
+def code_used():
+    return '', 409
+
+
+def code_bad():
+    return '', 412
+
+# utilities
+
+def to_iso8601(time):
+    """convert time to database friendly T Z format"""
+    return time.replace(tzinfo=None).isoformat() + 'Z'
--- tests/test_flask.py
+++ tests/test_flask.py
@@ -31,6 +31,22 @@ def test_shorten():
         res = c.post('/shorten', json={'url': 'https://www.energyworx.com/', 'shortcode': 'ewx123'})
         assert res.status_code == 201
 
+        content = res.get_json()
+        assert content['shortcode'] == 'ewx123'
+
+
+def test_shorten_random_url():
+    a = app.create_app()
+
+    with a.test_client() as c:
+        import re
+
+        res = c.post('/shorten', json={'url': 'https://www.energyworx.com/', 'shortcode': 'ewx123'})
+        assert res.status_code == 201
+
+        content = res.get_json()
+        assert re.match(r"^[a-zA-Z0-9_]{6}$", content['shortcode'])
+
 
 def test_shorten_no_url():
     a = app.create_app()
@@ -39,9 +55,85 @@ def test_shorten_no_url():
         res = c.post('/shorten', json={'shortcode': 'ewx123'})
         assert res.status_code == 400
 
+
 def test_shorten_badcode():
     a = app.create_app()
 
     with a.test_client() as c:
         res = c.post('/shorten', json={'url': 'https://www.energyworx.com/', 'shortcode': 'eWx1235'})
         assert res.status_code == 412
+
+
+def test_shorten_double_add():
+    a = app.create_app()
+
+    with a.test_client() as c:
+        res = c.post('/shorten', json={'url': 'https://www.energyworx.com/', 'shortcode': 'ewx123'})
+        assert res.status_code == 201
+
+        content = res.get_json()
+        assert content['shortcode'] == 'ewx123'
+
+        res = c.post('/shorten', json={'url': 'https://www.energyworx.com/', 'shortcode': 'ewx123'})
+        assert res.status_code == 409
+
+
+def test_unshorten():
+    from urllib.parse import urlparse
+
+    a = app.create_app()
+
+    with a.test_client() as c:
+        res = c.post('/shorten', json={'url': 'https://www.energyworx.com/', 'shortcode': 'ewx123'})
+        assert res.status_code == 201
+
+        content = res.get_json()
+        assert content['shortcode'] == 'ewx123'
+
+        res = c.get('/ewx123')
+        assert res.status_code == 302
+        assert res.location == 'https://www.energyworx.com/'
+
+
+def test_unshorten_invalid():
+    a = app.create_app()
+
+    with a.test_client() as c:
+        res = c.get('/ewx123')
+        assert res.status_code == 404
+
+
+def test_unshorten_stats():
+    a = app.create_app()
+
+    with a.test_client() as c:
+        res = c.post('/shorten', json={'url': 'https://www.energyworx.com/', 'shortcode': 'ewx123'})
+        assert res.status_code == 201
+
+        content = res.get_json()
+        assert content['shortcode'] == 'ewx123'
+
+        res = c.get('/ewx123/stats')
+        assert res.status_code == 200
+
+        content = res.get_json()
+        assert content['redirectCount'] == 0
+
+        # use it
+        res = c.get('/ewx123')
+
+        res = c.get('/ewx123/stats')
+        assert res.status_code == 200
+
+        content = res.get_json()
+        assert content['redirectCount'] == 1
+
+        # use twice
+        res = c.get('/ewx123')
+        res = c.get('/ewx123')
+
+        res = c.get('/ewx123/stats')
+        assert res.status_code == 200
+
+        content = res.get_json()
+        assert content['redirectCount'] == 3
